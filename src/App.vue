7d9fe33e89dbdc66424862992c859235aea1f8a0
zhouqihang <810236916@qq.com>
2017-10-16 10:12:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <markdown></markdown>
+  </div>
+</template>
+
+<script>
+  import Markdown from './components/Markdown';
+
+export default {
+    name: 'app',
+    components: {
+      Markdown,
+    },
+};
+</script>
+
+<style>
+#app {
+  width: 1000px;
+  padding:20px;
+}
+</style>
---
+++ src/components/Markdown.vue
@@ -0,0 +1,216 @@
+<template>
+  <div id="markdown" class="markdown markdown-default">
+    <div class="markdown-tools">
+      <ul class="markdown-tools-left">
+        <!--<li><i class="fa fa-header" aria-hidden="true" title="header"></i></li>-->
+        <li><i class="fa markdown-icon" aria-hidden="true" title="header 1" @click="handleText2Header(1)">H1</i></li>
+        <li><i class="fa markdown-icon" aria-hidden="true" title="header 2">H2</i></li>
+        <li><i class="fa markdown-icon" aria-hidden="true" title="header 3">H3</i></li>
+        <li><i class="fa markdown-icon" aria-hidden="true" title="header 4">H4</i></li>
+        <li><i class="fa markdown-icon" aria-hidden="true" title="header 5">H5</i></li>
+        <li><i class="fa markdown-icon" aria-hidden="true" title="header 6">H6</i></li>
+        <li><i class="fa markdown-icon markdown-slide-line" aria-hidden="true">|</i></li>
+        <li><i class="fa fa-bold" aria-hidden="true" title="bold" ></i></li>
+        <li><i class="fa fa-italic" aria-hidden="true" title="italic" ></i></li>
+        <li><i class="fa fa-strikethrough" aria-hidden="true" title="strikethrough" ></i></li>
+        <li><i class="fa fa-list-ol" aria-hidden="true" title="ol list" ></i></li>
+        <li><i class="fa fa-list-ul" aria-hidden="true" title="ul list" ></i></li>
+        <li><i class="fa fa-quote-right" aria-hidden="true" title="quote" ></i></li>
+        <li><i class="fa markdown-icon markdown-slide-line" aria-hidden="true">|</i></li>
+        <li><i class="fa fa-code" aria-hidden="true" title="code block" ></i></li>
+        <li><i class="fa fa-table" aria-hidden="true" title="table" ></i></li>
+        <li><i class="fa fa-picture-o" aria-hidden="true" title="upload picture" ></i></li>
+        <li><i class="fa fa-link" aria-hidden="true" title="link" ></i></li>
+        <li><i class="fa fa-minus" aria-hidden="true" title="halving line" ></i></li>
+        <li><i class="fa fa-clock-o" aria-hidden="true" title="current datetime" ></i></li>
+        <li><i class="fa fa-smile-o" aria-hidden="true" title="emoji" ></i></li>
+      </ul>
+      <ul class="markdown-tools-right">
+        <li><i class="fa fa-eye-slash" aria-hidden="true" title="close preview"></i></li>
+        <li><i class="fa fa-desktop" aria-hidden="true" title="full screen preview"></i></li>
+        <li><i class="fa fa-arrows-alt" aria-hidden="true" title="full screen"></i></li>
+      </ul>
+    </div>
+    <div class="markdown-content">
+      <div class="markdown-edit">
+        <textarea
+          class="markdown-edit-box"
+          v-model="markdownText"
+          ref="markdownText"
+          @keyup="inputing">
+        </textarea>
+      </div>
+      <div class="markdown-preview markdown-body" ref="markdownHTML"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+import 'github-markdown-css';
+import 'highlight.js/styles/github.css';
+import 'font-awesome/css/font-awesome.min.css';
+import hljs from 'highlight.js';
+
+
+export default {
+  name: 'markdown',
+  data() {
+    return {
+      markdownText: '',
+    };
+  },
+  computed: {
+    markdownHTML: {
+      get() {
+        return marked(this.markdownText);
+      },
+    },
+  },
+  methods: {
+    initDomVars() {
+      try {
+        this.markdownTextDom = this.$refs.markdownText;
+        this.markdownHTMLDom = this.$refs.markdownHTML;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    inputing() {
+      this.setMarkdownHTML();
+      this.highlightMarkdownHTML();
+      console.log(this.markdownTextDom.selectionStart);
+    },
+    setMarkdownHTML() {
+      try {
+        this.markdownHTMLDom.innerHTML = this.markdownHTML;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    highlightMarkdownHTML() {
+      let codes = [];
+      try {
+        codes = this.markdownHTMLDom.querySelectorAll('pre code');
+      } catch (e) {
+        console.error(e);
+      }
+      Array.from(codes)
+        .forEach(i => {
+          hljs.highlightBlock(i);
+        });
+    },
+    handleText2Header(headerNum) {
+      console.log(this.markdownTextDom.selectionStart, headerNum);
+    },
+  },
+  mounted() {
+    this.initDomVars();
+  },
+};
+</script>
+
+<style scoped type="text/css">
+  .markdown {
+    width: 100%;
+    height: 400px;
+    border: 1px solid #eeeff1;
+    box-sizing: border-box;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    overflow: hidden;
+    border-color: #000;
+  }
+  .markdown .markdown-icon {
+    font-weight: bold;
+    font-size: inherit;
+  }
+  .markdown .markdown-icon.markdown-slide-line {
+    cursor: default;
+    opacity: 0.7;
+  }
+  .markdown > .markdown-tools {
+    width: 100%;
+    height: 40px;
+    border-bottom: 1px solid #eeeff1;
+    line-height: 40px;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 0 15px;
+  }
+  .markdown > .markdown-tools > ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: inline-block;
+    overflow: hidden;
+  }
+  .markdown > .markdown-tools > ul > li {
+    float: left;
+  }
+  .markdown > .markdown-tools > ul > li:nth-child(n+2) {
+    margin-left: 15px;
+  }
+  .markdown > .markdown-tools > ul > li i {
+    cursor: pointer;
+  }
+  .markdown > .markdown-tools > ul > li i:hover {
+    opacity: 0.7;
+  }
+  .markdown > .markdown-tools > .markdown-tools-left {
+    float: left;
+  }
+  .markdown > .markdown-tools > .markdown-tools-right {
+    float: right;
+  }
+  .markdown > .markdown-content {
+    width: 100%;
+    height: calc(100% - 41px);
+    display: flex;
+  }
+  .markdown > .markdown-content > .markdown-edit {
+    flex: 1;
+  }
+  .markdown > .markdown-content > .markdown-edit > .markdown-edit-box {
+    width: 100%;
+    height: 100%;
+    resize: none;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    padding: 15px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    line-height: 1.8;
+    font-size: 14px;
+  }
+  .markdown > .markdown-content > .markdown-preview {
+    flex: 1;
+    padding: 15px;
+    overflow: auto;
+  }
+
+  /*github markdown css*/
+  .markdown-body {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  /*default theme*/
+  .markdown.markdown-default > .markdown-content > .markdown-edit {
+    border-right: 1px solid #eeeff1;
+    background-color: rgb(250, 251, 252);
+    color: rgb(101, 101, 101);
+  }
+  .markdown.markdown-default > .markdown-tools {
+    color: rgb(101, 101, 101);
+    background-color: rgb(250, 251, 252);
+  }
+</style>
