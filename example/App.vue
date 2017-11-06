b272454ccd8257440d7ac2e5c02ac1e56c279240
zhouqihang <console_log@126.com>
2017-11-06 14:45:40 +0800

    Add example

---
+++ example/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <vue-editor-markdown v-model="markdown"></vue-editor-markdown>
+    <p>{{markdown}}</p>
+  </div>
+</template>
+
+<script>
+import VueEditorMarkdown from '../src/main';
+
+export default {
+  name: 'app',
+  components: {
+    VueEditorMarkdown,
+  },
+  data() {
+    return {
+      markdown: {
+        markdown: '# vue-editor-markdown',
+        html: '',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+  #app {
+    width: 1000px;
+    padding: 20px;
+  }
+</style>
